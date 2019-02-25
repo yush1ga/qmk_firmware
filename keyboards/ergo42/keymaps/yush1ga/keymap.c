85dbc359044d40fc677e02d39fe3207fd54e9133
Yuki Shiga <y.shiga.91+yush1ga@gmail.com>
2019-02-26 03:40:55 +0900

    add volume setting, RGUI

--- keyboards/ergo42/keymaps/yush1ga/keymap.c
+++ keyboards/ergo42/keymaps/yush1ga/keymap.c
@@ -16,14 +16,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
    * | LSft | LSft |   Z  |   X  |   C  |   V  |  B   |   |  N   |   M  |   ,  |   .  |   /  |  UP  | Enter|
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | LCtrl| LAlt | LGUI |      | META2| META1|Space |   |Space | META1| META2|      | LEFT | DOWN | RIGHT|
+   * | LCtrl| LAlt | LGUI |      | META2| META1|Space |   |Space | META1| META2| RGUI | LEFT | DOWN | RIGHT|
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [BASE] = LAYOUT( \
-    KC_ESC,   KC_ESC,  KC_Q,    KC_W,    KC_E,      KC_R,      KC_T,   KC_Y,   KC_U,      KC_I,      KC_O,   KC_P,    KC_LBRC, KC_RBRC, \
-    KC_TAB,   KC_TAB,  KC_A,    KC_S,    KC_D,      KC_F,      KC_G,   KC_H,   KC_J,      KC_K,      KC_L,   KC_SCLN, KC_QUOT, KC_BSPC, \
-    KC_LSFT,  KC_LSFT, KC_Z,    KC_X,    KC_C,      KC_V,      KC_B,   KC_N,   KC_M,      KC_COMM,   KC_DOT, KC_SLSH, KC_UP,   KC_ENT, \
-    KC_LCTL,  KC_LALT, KC_LGUI, _______, MO(META2), MO(META1), KC_SPC, KC_SPC, MO(META1), MO(META2), KC_EQL, KC_LEFT, KC_DOWN, KC_RGHT \
+    KC_ESC,   KC_ESC,  KC_Q,    KC_W,    KC_E,      KC_R,      KC_T,   KC_Y,   KC_U,      KC_I,      KC_O,    KC_P,    KC_LBRC, KC_RBRC, \
+    KC_TAB,   KC_TAB,  KC_A,    KC_S,    KC_D,      KC_F,      KC_G,   KC_H,   KC_J,      KC_K,      KC_L,    KC_SCLN, KC_QUOT, KC_BSPC, \
+    KC_LSFT,  KC_LSFT, KC_Z,    KC_X,    KC_C,      KC_V,      KC_B,   KC_N,   KC_M,      KC_COMM,   KC_DOT,  KC_SLSH, KC_UP,   KC_ENT, \
+    KC_LCTL,  KC_LALT, KC_LGUI, _______, MO(META2), MO(META1), KC_SPC, KC_SPC, MO(META1), MO(META2), KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT \
   ),
 
   /* META1
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [META1] = LAYOUT( \
     _______, _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, \
-    _______, _______, XXXXXXX, KC_MHEN, KC_HENK, XXXXXXX, XXXXXXX,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_GRV,  KC_BSLS, \
+    _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_GRV,  KC_BSLS, \
     _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
     _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______ \
   ),
