e233ae7a5a4430aa8132f4891e587fe6d482463c
Yuki Shiga <y.shiga.91+yush1ga@gmail.com>
2019-02-26 03:42:04 +0900

    fix space between l and r

--- keyboards/ergo42/keymaps/yush1ga/keymap.c
+++ keyboards/ergo42/keymaps/yush1ga/keymap.c
@@ -20,10 +20,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [BASE] = LAYOUT( \
-    KC_ESC,   KC_ESC,  KC_Q,    KC_W,    KC_E,      KC_R,      KC_T,   KC_Y,   KC_U,      KC_I,      KC_O,    KC_P,    KC_LBRC, KC_RBRC, \
-    KC_TAB,   KC_TAB,  KC_A,    KC_S,    KC_D,      KC_F,      KC_G,   KC_H,   KC_J,      KC_K,      KC_L,    KC_SCLN, KC_QUOT, KC_BSPC, \
-    KC_LSFT,  KC_LSFT, KC_Z,    KC_X,    KC_C,      KC_V,      KC_B,   KC_N,   KC_M,      KC_COMM,   KC_DOT,  KC_SLSH, KC_UP,   KC_ENT, \
-    KC_LCTL,  KC_LALT, KC_LGUI, _______, MO(META2), MO(META1), KC_SPC, KC_SPC, MO(META1), MO(META2), KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT \
+    KC_ESC,   KC_ESC,  KC_Q,    KC_W,    KC_E,      KC_R,      KC_T,      KC_Y,   KC_U,      KC_I,      KC_O,    KC_P,    KC_LBRC, KC_RBRC, \
+    KC_TAB,   KC_TAB,  KC_A,    KC_S,    KC_D,      KC_F,      KC_G,      KC_H,   KC_J,      KC_K,      KC_L,    KC_SCLN, KC_QUOT, KC_BSPC, \
+    KC_LSFT,  KC_LSFT, KC_Z,    KC_X,    KC_C,      KC_V,      KC_B,      KC_N,   KC_M,      KC_COMM,   KC_DOT,  KC_SLSH, KC_UP,   KC_ENT, \
+    KC_LCTL,  KC_LALT, KC_LGUI, _______, MO(META2), MO(META1), KC_SPC,    KC_SPC, MO(META1), MO(META2), KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT \
   ),
 
   /* META1
